202bf5f46ae8daaba52d15299740d78d85a3c606
student <student@domain.local>
2018-04-23 10:31:57 +0200

    V3.0 database start

--- app/src/main/java/jasic/filip/chatapplication/Contact.java
+++ app/src/main/java/jasic/filip/chatapplication/Contact.java
@@ -2,18 +2,28 @@ package jasic.filip.chatapplication;
 import android.graphics.drawable.Drawable;
 
 public class Contact {
-    public String mNameFirst;
-    public String mName;
+    public String mFirstLetter;
+    public String mFirstName;
+    public String mLastName;
+    public String mUsername;
+    public String mContactId;
     public Drawable mImage;
 
 
-    Contact(String nameFirst, String name, Drawable drawable) {
-        mNameFirst=nameFirst;
-        mName = name;
+    Contact(String firstLetter, String firstName, Drawable drawable,String lastName,String userName,String contactId) {
+        mFirstLetter=firstLetter;
+        mFirstName = firstName;
+        mLastName=lastName;
+        mUsername=userName;
+        mContactId=contactId;
         mImage = drawable;
+
     }
 
     public String getName(){
-        return mName;
+        return mFirstName;
     }
+    public String getLastName(){return  mLastName;}
+    public String getUsername(){return mUsername;}
+    public String getContactId(){return mContactId;}
 }
--- app/src/main/java/jasic/filip/chatapplication/Message.java
+++ app/src/main/java/jasic/filip/chatapplication/Message.java
@@ -3,16 +3,23 @@ package jasic.filip.chatapplication;
 public class Message {
     public String mMessage;
     public String mBot;
+    public String mMesssageId;
+    public String mReciverId;
+    public String mSenderId;
 
-    public Message(String message,String bot){
+    public Message(String message,String bot,String messageId,String reciverId,String senderId){
         mMessage=message;
         mBot=bot;
+        mMesssageId=messageId;
+        mReciverId=reciverId;
+        mSenderId=senderId;
     }
 
     public String getMessage(){
         return mMessage;
     }
-
+    public String getReciverId() {return mReciverId;}
+    public String getMesssageId(){return mMesssageId;}
     public String getBot() {
         return mBot;
     }
